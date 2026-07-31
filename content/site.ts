36b34d1cf2c3b49ff6f867ce227eeda4dd863de7
Nicolás Correa <89622611+ncorrea-13@users.noreply.github.com>
2026-07-31 17:06:55 -0300

    fix: errores menores de redirecciones en el homeserver

--- content/site.ts
+++ content/site.ts
@@ -1,6 +1,7 @@
-export const SERVIDOR_URL = "https://ncorrea-homeserver.tail383faf.ts.net";
+export const SERVIDOR_URL = "https://homeserver.ncorrea.com.ar/";
 
 export const EMAIL = "ncorrea13@proton.me";
-export const LINKEDIN_URL = "https://www.linkedin.com/in/nicolas-correa-serrat/";
+export const LINKEDIN_URL =
+  "https://www.linkedin.com/in/nicolas-correa-serrat/";
 export const WHATSAPP_URL = "https://wa.me/5492612582481";
 export const GITHUB_URL = "https://github.com/ncorrea-13";
