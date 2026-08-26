110497a93c8d951189ddb0356862bb459e53d319
Nicolás Correa <89622611+ncorrea-13@users.noreply.github.com>
2026-08-25 23:46:06 -0300

    chore: eliminado service/

--- service/write-boot-time.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-: "${DOC_ROOT:?DOC_ROOT no definido (revisar EnvironmentFile en el .service o el .env sourceado)}"
-
-BOOT_EPOCH=$(date -d "$(uptime -s)" +%s)
-printf '{"boot_epoch": %s}\n' "$BOOT_EPOCH" > "$DOC_ROOT/boot.json"
